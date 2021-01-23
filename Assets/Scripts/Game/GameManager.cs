a89d3bf92187bdf6b358b38abc6ab66c44b28d64
Ted Mundy <pepper0161@gmail.com>
2021-01-23 19:26:22 +0000

    Changed basically everything
    - Made Object.Spawn() actually do something
    - Added the object whitelist
    - Added Debug Mode
    - Changed the light intensity
    - Changed the rotation (i think)
    - Changed the smoothness of bowling_ball.mat
    - Did not move the bowling ball I don't think

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     /// <summary>
-    /// The list of allowed GameObjects to turn into Objects.
+    /// The list of the names of allowed GameObjects to turn into Objects.
     /// </summary>
-    public static List<GameObject> allowedObjects { get; private set; }
+    public static List<string> allowedObjects { get; private set; } = new List<string>();
     /// <summary>
     /// Whether or not the game is in Debug Mode (bypass restrictions).
     /// </summary>
-    // TODO: Change this to false.
-    public static bool debugMode { get; private set; } = true;
+    public static bool debugMode { get; private set; } = false;
     private void Start() {
-        // TODO: Read from a file and set the allowedObjects list.
+        string dir = Path.Combine(Application.persistentDataPath, "hub");
+        string path = Path.Combine(Application.persistentDataPath, "hub", "whitelistobjects.txt");
+        if (!Directory.Exists(dir)) {
+            Directory.CreateDirectory(dir);
+        }
+        if (!File.Exists(path)) {
+            Debug.Log("Creating whitelist file");
+            // For some reason, we have to create the file and then close it.
+            FileStream f = File.Create(path);
+            f.Close();
+            Debug.Log("Whitelist file complete! Writing all GameObjects...");
+            #region Objects array
+            // THERE IS NO OTHER WAY TO DO THIS
+            // TODO: Try and find a better way to do this.
+            string[] toWrite = {
+                "bowling_ball"
+            };
+            #endregion
+            File.WriteAllLines(path, toWrite);
+        }
+        string[] objects = File.ReadAllLines(path);
+        foreach (string obj in objects) {
+            allowedObjects.Add(obj);
+        }
+
+        Object.Spawn(GameObject.Find("bowling_ball"), new Vector3(0, 0), Quaternion.Euler(new Vector3(0, 0)));
     }
 }
--- Assets/Scripts/Object/Object.cs
+++ Assets/Scripts/Object/Object.cs
@@ -19,17 +19,30 @@ public class Object {
     /// <param name = "toClone">The GameObject to clone.</param>
     public Object(GameObject toClone) {
         if (!GameManager.debugMode) {
-            // TODO: Check if it's valid.
+            this.isValid = GameManager.allowedObjects.Contains(toClone.name);
         }
     }
     /// <summary>
-    /// Spawns an object
+    /// Spawns an object.
     /// </summary>
     /// <param name = "obj">The GameObject to spawn.</param>
+    /// <param name = "position">The position to spawn the Object.</param>
+    /// <param name = "rotation">The rotation to spawn the Object with.</param>
+    /// <param name = "inWorldSpace">Whether or not the Object is spawned in world space.</param>
     /// <returns>The object created, null if not.</returns>
-    public static Object Spawn(GameObject obj) {
-        // TODO: Spawn the object after checking if it's valid.
+    public static Object Spawn(GameObject obj, Vector3 position, Quaternion rotation, bool inWorldSpace = false) {
         Object created = new Object(obj);
+        if (created.isValid) {
+            created.gameObject = UnityEngine.Object.Instantiate(obj);
+            created.gameObject.name = obj.name;
+            if (inWorldSpace) {
+                created.gameObject.transform.position = position;
+                created.gameObject.transform.rotation = rotation;
+            } else {
+                created.gameObject.transform.localPosition = position;
+                created.gameObject.transform.localRotation = rotation;
+            }
+        }
         return created.isValid ? created : null;
     }
 }
